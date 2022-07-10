06fa8a76b5564513eaa6a827b1ee8efd38055d98
SeongHyeok <gottaa@naver.com>
2022-07-10 20:05:39 +0900

    Test commit

--- fromEC2/server.js
+++ fromEC2/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
@@ -78,9 +77,6 @@ io.on('connection', (socket) => {
 
 });
 
-
-});
-
 server.listen(3000, function () {
     console.log('Listening on http://localhost:3000/');
 });
@@ -90,7 +86,6 @@ function randomNum(min, max) {
     return randNum;
 }
 
-=======
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
@@ -100,11 +95,11 @@ var mysql = require('mysql');
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.set('view engine', 'ejs'); // 렌더링 엔진 모드를 ejs로 설정
-app.set('views',  __dirname + '/views');    // ejs이 있는 폴더를 지정
+app.set('views', __dirname + '/views');    // ejs이 있는 폴더를 지정
 
 app.get('/', (req, res) => {
     res.render('index');    // index.ejs을 사용자에게 전달
@@ -118,7 +113,7 @@ app.get('/sign_up', (req, res) => {
     res.render('sign_up');    // index.ejs을 사용자에게 전달
 })
 
-app.post('/login', function(req,res){
+app.post('/login', function (req, res) {
     console.log("post request (from '/sign_up' to '/login') ");
     console.log("id :", req.body.id);
     console.log("password :", req.body.password);
@@ -130,9 +125,9 @@ app.post('/login', function(req,res){
 
 
 
-io.on('connection', (socket) => {   
+io.on('connection', (socket) => {
     //연결이 들어오면 실행되는 이벤트
-    //socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.    
+    //socket 변수에는 실행 시점에 연결한 상대와 연결된 소켓의 객체가 들어있다.
     //socket.emit으로 현재 연결한 상대에게 신호를 보낼 수 있다.
     socket.emit('usercount', io.engine.clientsCount);
 
@@ -145,11 +140,11 @@ io.on('connection', (socket) => {
         io.emit('message', msg);
     });
 
-    socket.on('request_data', (trash_temp) => { 
+    socket.on('request_data', (trash_temp) => {
         // <-- 웹브라우저가 데이터 요청시 실행되는 서버함수
         //웹브라우저가 데이터요청(request_data)시 아래 코드처럼 DB에 연결해서 데이터를 받아온다.
         //그런뒤에 다시 웹브라우저에게 데이터를 던져줄려 했으나....
-        //지금 ec2가 죽어서....... 더이상 진행불가        
+        //지금 ec2가 죽어서....... 더이상 진행불가
         /* //지금당장은 데이터베이스가 없어서 주석처리
         var conn = mysql.createConnection({
             host: "db-rvsm.cudjy2uvoaml.ap-northeast-2.rds.amazonaws.com",
@@ -160,8 +155,8 @@ io.on('connection', (socket) => {
         });
         conn.connect(); //db접속
         */
-        
-        var data = randomNum(0,10);
+
+        var data = randomNum(0, 10);
 
         socket.emit('response_data', data);
         console.log('[server]data received: ' + data);
@@ -173,14 +168,11 @@ io.on('connection', (socket) => {
 
 });
 
-server.listen(3000, function() {
-  console.log('Listening on http://localhost:3000/');
+server.listen(3000, function () {
+    console.log('Listening on http://localhost:3000/');
 });
 
-function randomNum(min, max)
-{ 
-    var randNum = Math.floor(Math.random()*(max-min+1)) + min;
-    return randNum; 
+function randomNum(min, max) {
+    var randNum = Math.floor(Math.random() * (max - min + 1)) + min;
+    return randNum;
 }
-
->>>>>>> 34d41c3270d6bbe96a574e4bca312d19f4d14ee1
